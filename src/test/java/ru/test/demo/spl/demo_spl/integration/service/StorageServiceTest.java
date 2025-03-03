20c413b2f56b7fbb90841e8d9576425b2b28740f
Ivansh96 <isavsin5@gmail.com>
2025-03-03 16:06:09 +0700

    integration testing

--- src/test/java/ru/test/demo/spl/demo_spl/integration/service/StorageServiceTest.java
+++ src/test/java/ru/test/demo/spl/demo_spl/integration/service/StorageServiceTest.java
@@ -1,16 +1,19 @@
 package ru.test.demo.spl.demo_spl.integration.service;
 
-import org.jooq.*;
 import org.jooq.Record;
+import org.jooq.*;
 import org.jooq.impl.DSL;
 import org.jooq.impl.SQLDataType;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jooq.JooqTest;
 import org.springframework.test.context.ActiveProfiles;
 import ru.test.demo.spl.demo_spl.configuration.Properties;
 
+import java.util.Date;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,13 +23,55 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @JooqTest
 @ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class StorageServiceTest {
 
     @Autowired
     private Properties properties;
     @Autowired
     private DSLContext dslContext;
+    Table<Record> table;
+    UUID id;
+    Long receivedAt;
+    Long processedAt;
+    Field<UUID> idField;
+    Field<Long> recievedAtField;
+    Field<Long> processedAtField;
+    Field<Integer> versionField;
+    Field<String> deviceVendorField;
+    Field<String> deviceProductField;
+    Field<String> deviceVersionField;
+    Field<String> eventClassIdField;
+    Field<String> nameField;
+    Field<Integer> severityField;
 
+
+    @BeforeAll
+    void generateVariables() {
+        id = UUID.randomUUID();
+        receivedAt = (new Date()).getTime();
+        processedAt = (new Date()).getTime() + 10;
+    }
+
+    @BeforeAll
+    void initTable() {
+        table = DSL.table(properties.getIntegration().getEventTableName());
+    }
+
+    @BeforeAll
+    void addFields() {
+        idField = field(name("b_id"), SQLDataType.UUID);
+        recievedAtField = field(name("b_receivedAt"), SQLDataType.BIGINT);
+        processedAtField = field(name("b_processedAt"), SQLDataType.BIGINT);
+
+        versionField = field(name("c_version"), SQLDataType.INTEGER);
+        deviceVendorField = field(name("c_deviceVendor"), SQLDataType.VARCHAR(255));
+        deviceProductField = field(name("c_deviceProduct"), SQLDataType.VARCHAR(255));
+        deviceVersionField = field(name("c_deviceVersion"), SQLDataType.VARCHAR(255));
+        eventClassIdField = field(name("c_eventClassId"), SQLDataType.VARCHAR(255));
+        nameField = field(name("c_name"), SQLDataType.VARCHAR(255));
+        severityField = field(name("c_severity"), SQLDataType.INTEGER);
+    }
 
     @Test
     void testDSLContext() {
@@ -35,29 +80,14 @@ public class StorageServiceTest {
     }
 
     @Test
-    @DisplayName("Creating table")
+    @DisplayName("Create table")
     void getDefaultTableNameFromProperties() {
-
-        Table<Record> table = DSL.table(properties.getIntegration().getEventTableName());
         assertEquals("main", table.getName());
-
     }
 
     @Test
-    @DisplayName("Creating table")
+    @DisplayName("Create table")
     void getFieldsNameAndType() {
-
-        Field<UUID> idField = field(name("b_id"), SQLDataType.UUID);
-        Field<Long> recievedAtField = field(name("b_receivedAt"), SQLDataType.BIGINT);
-        Field<Long> processedAtField = field(name("b_processedAt"), SQLDataType.BIGINT);
-
-        Field<Integer> versionField = field(name("c_version"), SQLDataType.INTEGER);
-        Field<String> deviceVendorField = field(name("c_deviceVendor"), SQLDataType.VARCHAR(255));
-        Field<String> deviceProductField = field(name("c_deviceProduct"), SQLDataType.VARCHAR(255));
-        Field<String> deviceVersionField = field(name("c_deviceVersion"), SQLDataType.VARCHAR(255));
-        Field<String> eventClassIdField = field(name("c_eventClassId"), SQLDataType.VARCHAR(255));
-        Field<String> nameField = field(name("c_name"), SQLDataType.VARCHAR(255));
-        Field<Integer> severityField = field(name("c_severity"), SQLDataType.INTEGER);
 
         assertEquals("b_id", idField.getName());
         assertEquals(SQLDataType.UUID, idField.getDataType());
@@ -91,23 +121,10 @@ public class StorageServiceTest {
 
 
     }
+
     @Test
-    @DisplayName("Creating table")
+    @DisplayName("Create table")
     void getFullTable() {
-
-        Table<Record> table = DSL.table(properties.getIntegration().getEventTableName());
-
-        Field<UUID> idField = field(name("b_id"), SQLDataType.UUID);
-        Field<Long> recievedAtField = field(name("b_receivedAt"), SQLDataType.BIGINT);
-        Field<Long> processedAtField = field(name("b_processedAt"), SQLDataType.BIGINT);
-
-        Field<Integer> versionField = field(name("c_version"), SQLDataType.INTEGER);
-        Field<String> deviceVendorField = field(name("c_deviceVendor"), SQLDataType.VARCHAR(255));
-        Field<String> deviceProductField = field(name("c_deviceProduct"), SQLDataType.VARCHAR(255));
-        Field<String> deviceVersionField = field(name("c_deviceVersion"), SQLDataType.VARCHAR(255));
-        Field<String> eventClassIdField = field(name("c_eventClassId"), SQLDataType.VARCHAR(255));
-        Field<String> nameField = field(name("c_name"), SQLDataType.VARCHAR(255));
-        Field<Integer> severityField = field(name("c_severity"), SQLDataType.INTEGER);
 
         dslContext.createTableIfNotExists(table)
                 .column(idField)
@@ -146,4 +163,52 @@ public class StorageServiceTest {
     }
 
 
+    @Test
+    @DisplayName("Insert entry")
+    void getBasicFieldsValues() {
+
+        dslContext.insertInto(table)
+                .set(idField, id)
+                .set(recievedAtField, receivedAt)
+                .set(processedAtField, processedAt)
+                .execute();
+
+        final Result<Record3<UUID, Long, Long>> result =
+                dslContext.select(idField, recievedAtField, processedAtField)
+                        .from(table)
+                        .fetch();
+
+        assertEquals(id, result.getValue(0, idField));
+        assertEquals(receivedAt, result.getValue(0, recievedAtField));
+        assertEquals(processedAt, result.getValue(0, processedAtField));
+    }
+
+    @Test
+    @DisplayName("Insert entry")
+    void getFieldsValues() {
+
+        dslContext.insertInto(table)
+                .set(versionField, 0)
+                .set(deviceVendorField, "fil-it.ru")
+                .set(deviceProductField, "segment-transfer-ms-main")
+                .set(deviceVersionField, "1.6.0-SNAPSHOT")
+                .set(eventClassIdField, "login error")
+                .set(nameField, "KeycloakAccessToken")
+                .set(severityField, 3)
+                .execute();
+
+        final Result<Record7<Integer, String, String, String, String, String, Integer>> result =
+                dslContext.select(versionField, deviceVendorField, deviceProductField, deviceVersionField,
+                                eventClassIdField, nameField, severityField)
+                        .from(table)
+                        .fetch();
+
+        assertEquals(0, result.getValue(0, versionField));
+        assertEquals("fil-it.ru", result.getValue(0, deviceVendorField));
+        assertEquals("segment-transfer-ms-main", result.getValue(0, deviceProductField));
+        assertEquals("1.6.0-SNAPSHOT", result.getValue(0, deviceVersionField));
+        assertEquals("login error", result.getValue(0, eventClassIdField));
+        assertEquals("KeycloakAccessToken", result.getValue(0, nameField));
+        assertEquals(3, result.getValue(0, severityField));
+    }
 }
